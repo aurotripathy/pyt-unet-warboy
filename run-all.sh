c72642ee9dcd7f00cb8c00710d9ebd67d4816670
Auro Tripathy <ipserv@yahoo.com>
2023-06-13 01:20:30 +0900

    init

--- run-all.sh
+++ run-all.sh
@@ -5,9 +5,12 @@ if [[ $1 -eq 0 ]]
 then
     echo "Usage: $0 <batch-size>"
 else
+    echo "+++Converting to ONNX"
     ./convert_2_onnx.py --bs $1
 
+    echo "+++Quantizing from ONNX"
     ./quantize.py --bs $1
 
+    echo "+++Compiling and running"
     ./async_compile_run.py --bs $1
 fi
